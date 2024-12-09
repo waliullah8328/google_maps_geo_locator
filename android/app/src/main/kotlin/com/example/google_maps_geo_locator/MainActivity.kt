e5c93ef213dc039036dfafa580b3993b157759ec
Wali Ullah Ripon <69679369+waliullah8328@users.noreply.github.com>
2024-12-09 09:54:35 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/google_maps_geo_locator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.google_maps_geo_locator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+        centerTitle: true,
+      ),
+body:  const Center(child: Text("My Current Location")),
+    );
+  }
+}
